73f69b6a0d648e7f6fe7dd4551ba5d4032ccd6bb
Phung Hong Phuoc <phunghongphuoc178@gmail.com>
2023-11-13 14:46:39 +0700

    Hoan thien

---
+++ index/New folder/test.php
@@ -0,0 +1,14 @@
+<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js' type='text/javascript'></script>
+<script type="text/javascript" src="../../js/jquery-1.11.3.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		function clickme()
+		{
+			$('.alo').click(function(){
+				$('alop').fadeIn();
+				});
+		};
+    });
+</script>
+<div class="alo" onClick="clickme()">xin chao</div>
+<div class="alop" >aloooooooooooooooooooooo</div>
